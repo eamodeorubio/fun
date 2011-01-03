013b1e0d24c1b947dc344d8d405ac2f81d2427c1
Enrique J. Amodeo Rubio <eamodeorubio@gmail.com>
2011-01-03 21:42:08 +0100

    Roman number test for V and trivial implementation

--- ruby_fun/romans.rb
+++ ruby_fun/romans.rb
@@ -1,5 +1,6 @@
 class Fixnum 
 	def to_roman
+		return 'V' if self == 5
 		'I'
 	end
 end
@@ -7,5 +8,9 @@ end
 describe "Roman number" do
 	it "I is equivalent to 1" do 
 		1.to_roman.should == 'I'
+	end
+
+	it "V is equivalent to 5" do
+		5.to_roman.should == 'V'
 	end
 end
